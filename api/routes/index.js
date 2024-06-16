2d28d8981695301b9a56a1504e49b17a56e12151
Mustafa Zorlu <mzorlu3579@gmail.com>
2024-06-16 17:04:13 +0300

    dotenv settings

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const config = require("../config/index.js");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+    res.render("index", { title: "Express", config: config });
 });
 
 module.exports = router;
